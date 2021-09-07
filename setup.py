1de51f32d36ec58417b1667401e6dced0ce2b6bf
JamFfm <Jan.Battermann@t-online.de>
2021-09-07 22:55:04 +0200

    indents

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, find_packages
 
 # read the contents of your README file
 from os import path
+
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
@@ -17,13 +18,13 @@ setup(name='cbpi4-LCDisplay',
       include_package_data=True,
       package_data={
           # If any package contains *.txt or *.rst files, include them:
-      	  '': ['*.txt', '*.rst', '*.yaml'],
-      	  'cbpi4-LCDisplay': ['*','*.txt', '*.rst', '*.yaml']},
+          '': ['*.txt', '*.rst', '*.yaml'],
+          'cbpi4-LCDisplay': ['*', '*.txt', '*.rst', '*.yaml']},
       # packages=['cbpi4-LCDisplay'],
       install_requires=[
           'cbpi>=4.0.0.33',
-	  'smbus2',
+          'smbus2',
       ],
       long_description=long_description,
       long_description_content_type='text/markdown'
-     )
+      )
