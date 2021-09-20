8bfcf3e95433b324d9c0b37063e66e0e7fe2298d
JamFfm <Jan.Battermann@t-online.de>
2021-09-20 23:14:44 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -7,22 +7,23 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='cbpi4-LCDisplay',
-      version='5.0.8',
+      version='5.0.9',
       description='CraftBeerPi4 LCD Plugin',
       author='Jan Battermann',
-      author_email='jan.battermann@t-online.de',
+      author_email='Jan.Battermann@t-online.de',
       url='https://github.com/JamFfm/cbpi4-LCDisplay',
       license='GPLv3',
-      packages=find_packages(),
       include_package_data=True,
       package_data={
           # If any package contains *.txt or *.rst files, include them:
           '': ['*.txt', '*.rst', '*.yaml'],
           'cbpi4-LCDisplay': ['*', '*.txt', '*.rst', '*.yaml']},
       # packages=['cbpi4-LCDisplay'],
+      packages=find_packages(),
       install_requires=[
             'cbpi>=4.0.0.33',
             'smbus2',
+            'RPLCD',
       ],
       long_description=long_description,
       long_description_content_type='text/markdown'
